92ea860c3d6f9e141ecc0670a9db20980daf8e54
goodjun <goodjun@163.com>
2021-07-13 14:40:54 +0800

    feat: update router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,44 +5,26 @@ import GoogleAuthHandle from "../pages/login/google-auth-handle";
 import JoinMeeting from "../pages/join-meeting";
 import Meeting from "../pages/meeting";
 import NotFound from "../pages/not-found";
-import { AuthContext } from "../contexts/auth-context";
-
-const NotAuthRouter = () => {
-  return (
-    <>
-      <Route key="login" path="/login" component={Login} exact />
-      <Route
-        key="google-redirect-uri"
-        path="/"
-        component={GoogleAuthHandle}
-        exact
-      />
-    </>
-  );
-};
-
-const AuthRouter = () => {
-  return (
-    <>
-      <Route
-        key="join-meeting"
-        path="/join-meeting"
-        component={JoinMeeting}
-        exact
-      />
-      <Route key="meeting" path="/meeting" component={Meeting} exact />
-    </>
-  );
-};
 
 export const Router = () => {
-  const { isLogin } = React.useContext(AuthContext);
-
   return (
     <BrowserRouter forceRefresh={true}>
       <Switch>
-        {!isLogin ? <NotAuthRouter /> : <AuthRouter />}
-        <Route path="/not-found" component={NotFound} />
+        <Route
+          key="join-meeting"
+          path="/join-meeting"
+          component={JoinMeeting}
+          exact
+        />
+        <Route key="login" path="/login" component={Login} exact />
+        <Route
+          key="google-redirect-uri"
+          path="/"
+          component={GoogleAuthHandle}
+          exact
+        />
+        <Route key="meeting" path="/meeting" component={Meeting} exact />
+        <Route path="/not-found" component={NotFound} exact />
         <Redirect to="/not-found" />
       </Switch>
     </BrowserRouter>
